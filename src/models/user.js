2b9b39c1a9bf4dbacf288c699ce40966982625d7
martinezpke <martinezpke26@gmail.com>
2024-04-19 16:52:44 -0500

    modificaciones con del modelo de usuario (atributos)

--- src/models/user.js
+++ src/models/user.js
@@ -1,40 +1,58 @@
 /* const db = require('../db/connection');
 const encode = require('../utils/encode') */
 
-import db from '../db/connection.js';
-import encode from '../utils/encode.js';
+import db from '../db/connection.js'
+import encode from '../utils/encode.js'
 
-const createUser = async (username, name, lastName, country, email, password) => {
-  const [rows] = await db.promise().query('INSERT INTO users (username, name, last_name, country, email, password) VALUES (?, ?, ?, ?, ?, ? )', [username, name, lastName, country, email, password]);
-  return rows.insertId;
-};
-
+const createUser = async (
+  username,
+  name,
+  lastName,
+  country,
+  email,
+  password
+) => {
+  const [rows] = await db.promise().query(
+    `
+    INSERT INTO users (username, nombre, apellido, pais, email, password, tipo_cuenta, created_at, last_modified) 
+    VALUES (?, ?, ?, ?, ?, ?, ?, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)
+  `,
+    [username, name, lastName, country, email, password, 'admin']
+  )
+  return rows.insertId
+}
 
 const getUserByUsername = async (username) => {
-  const [rows] = await db.promise().query('SELECT id, username, name, last_name, country, email FROM users WHERE username = ?', [username]);
-  if (rows.length === 0){
-    return "Este usuario no existe";
+  const [rows] = await db
+    .promise()
+    .query(
+      'SELECT id, username, nombre, apellido, pais, email FROM users WHERE username = ?',
+      [username]
+    );
+  if (rows.length === 0) {
+    return 'Este usuario no existe';
   }
   return rows[0];
 };
 
-
 const validateUser = async (username, password) => {
-    const [rows] = await db.promise().query('SELECT id, username, password FROM users WHERE username = ?', [username]);
-    if (rows.length === 0){
-      return null; // No existe
-    }
-    
-    const storedPassword = rows[0].password;
-    const passMatches = await encode.compare(password, storedPassword);
- 
-    if(passMatches){
-      return true;
-    } else {
-      return null; // no coinciden
-    }
+  const [rows] = await db
+    .promise()
+    .query('SELECT id, username, password FROM users WHERE username = ?', [
+      username
+    ])
+  if (rows.length === 0) {
+    return null // No existe
+  }
+
+  const storedPassword = rows[0].password
+  const passMatches = await encode.compare(password, storedPassword)
+
+  if (passMatches) {
+    return true
+  } else {
+    return null // no coinciden
+  }
 }
 
-
-
-export { createUser, getUserByUsername, validateUser };
+export { createUser, getUserByUsername, validateUser }
